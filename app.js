9d5c922f72b41616d4b43bfcb24ce43cfeb68e01
Alley Rubadeau <Alley@Alleys-MacBook-Pro.local>
2015-07-23 12:26:23 -0600

    got api working at /auth/fitbit route

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+require('dotenv').load()
+var cookieSession = require('cookie-session')
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
@@ -5,10 +7,22 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+var FitbitStrategy = require('passport-fitbit').Strategy
+var passport = require('passport')
+
+app.set('trust proxy', 1); // trust first proxy
+
+app.use(cookieSession({
+  name: 'session',
+  keys: ['SECRET1', 'SECRET2']
+}));
+
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,9 +35,46 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.session({ secret: 'keyboard cat' }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+  done(null, obj);
+});
+
+
 
 app.use('/', routes);
 app.use('/users', users);
+
+passport.use(new FitbitStrategy({
+    consumerKey: process.env.KEY,
+    consumerSecret: process.env.SECRET,
+    callbackURL: "http://localhost:3000/auth/fitbit/callback"
+  },
+  function(token, tokenSecret, profile, done) {
+    User.findOrCreate({ fitbitId: profile.id }, function (err, user) {
+      return done(err, user);
+    });
+  }
+));
+
+app.get('/auth/fitbit',
+  passport.authenticate('fitbit'));
+
+app.get('/auth/fitbit/callback',
+  passport.authenticate('fitbit', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/');
+  });
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
