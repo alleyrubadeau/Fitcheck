fb1dbf581f3e2c57cabcd726dc9c68bdb3995cf3
Alley Rubadeau <Alley@Alleys-MacBook-Pro.local>
2015-07-27 11:13:48 -0600

    fitbit profile showing

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 require('dotenv').load()
-
+var session = require('express-session');
 var bcrypt = require('bcrypt')
-var cookieSession = require('cookie-session')
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
@@ -12,26 +11,16 @@ var Fitbit = require('fitbit')
 
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
-
 var passport = require('passport')
 var FitbitStrategy = require('passport-fitbit').Strategy
 
 
 var app = express();
 
-
-
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 app.set('trust proxy', 1); // trust first proxy
-
-app.use(cookieSession({
-  name: 'session',
-  keys: ['SECRET1', 'SECRET2']
-}));
 
 
 
@@ -42,138 +31,71 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-// app.use(express.session({ secret: 'keyboard cat' }));
+app.use(session({ secret: 'keyboard cat', resave: true, saveUninitialized: false }));
 app.use(passport.initialize());
 app.use(passport.session());
+
+
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+  done(null, obj);
+});
 
 passport.use(new FitbitStrategy({
     consumerKey: process.env.KEY,
     consumerSecret: process.env.SECRET,
     callbackURL: process.env.HOST,
-    scope: ['r_emailaddress', 'r_basicprofile'],
-    state: true
   },
   function(token, tokenSecret, profile, done) {
-    console.log(profile)
-    // User.findOrCreate({ fitbitId: profile.id }, function (err, user) {
-      return done(null, {
-      id: profile.id,
-      displayName: profile.displayName,
-      // photo: profile.photos[0],
-      token: accessToken
+    // asynchronous verification, for effect...
+    process.nextTick(function () {
+
+      // To keep the example simple, the user's Fitbit profile is returned to
+      // represent the logged-in user.  In a typical application, you would want
+      // to associate the Fitbit account with a user record in your database,
+      // and return that user instead.
+      console.log(profile);
+      return done(null, profile);
     });
   }
 ));
 
-app.get('/auth/fitbit', function (req, res) {
-  // Create an API client and start authentication via OAuth
-  var client = new Fitbit(process.env.KEY, process.env.SECRET);
-
-  client.getRequestToken(function (err, token, tokenSecret) {
-    if (err) {
-      res.redirect('/')
-      // Take action
-      // return;
-    }
-
-    req.session.oauth = {
-        requestToken: token
-      , requestTokenSecret: tokenSecret
-    };
-    res.redirect(client.authorizeUrl(token));
-  });
-});
-
-// app.get('/auth/fitbit', function (req, res) {
-//   var verifier = req.query.oauth_verifier
-//     , oauthSettings = req.session.oauth
-//     , client = new Fitbit(process.env.KEY, process.env.SECRET);
-//
-//   // Request an access token
-//   client.getAccessToken(
-//       oauthSettings.requestToken
-//     , oauthSettings.requestTokenSecret
-//     , verifier
-//     , function (err, token, secret) {
-//         if (err) {
-//           // Take action
-//           return;
-//         }
-//
-//         oauthSettings.accessToken = token;
-//         oauthSettings.accessTokenSecret = secret;
-//
-//         res.redirect('/stats');
-//       }
-//   );
-// });
-
-
-
 app.get('/auth/fitbit',
-  passport.authenticate('fitbit', {state: 'SOME STATE'}),
-  function(req, res) {
-    res.redirect('/')
+  passport.authenticate('fitbit'),
+  function(req, res){
+    // The request will be redirected to Fitbit for authentication, so this
+    // function will not be called.
   });
 
-  app.get('/auth/fitbit/callback',
-    passport.authenticate('fitbit', { failureRedirect: '/login' }),
-    function(req, res) {
-      res.redirect('/');
-    });
-
-    passport.serializeUser(function(user, done) {
-      done(null, user);
-    });
-
-    passport.deserializeUser(function(obj, done) {
-      done(null, obj);
-    });
+// GET /auth/fitbit/callback
+//   Use passport.authenticate() as route middleware to authenticate the
+//   request.  If authentication fails, the user will be redirected back to the
+//   login page.  Otherwise, the primary route function function will be called,
+//   which, in this example, will redirect the user to the home page.
+app.get('/auth/fitbit/callback',
+  passport.authenticate('fitbit', { failureRedirect: '/login' }),
+  function(req, res) {
+    res.redirect('/');
+  });
 
 
-    app.use(function (req, res, next) {
-      res.locals.user=req.user;
-      next();
-    })
-    app.use('/', routes);
-    app.use('/', users);
+app.use(function (req, res, next) {
+  res.locals.user = req.user;
+  next();
+})
 
-    app.use('/users/:id', function (req, res) {
-      res.send('user', + req.params.id)
-    })
-    app.use('/stats', function (req, res) {
-      res.render('stats', {user: req.user})
-    })
-
+app.use('/', routes);
+app.use('/:id', function (req, res) {
+  // res.send('user', + req.params.id)
+  res.send('user' + req.params.id + 'Total steps today: ');
+})
 
 app.get('/account', ensureAuthenticated, function(req, res){
   res.render('account', { user: req.user });
 });
-
-
-app.get('/:id/stats', function (req, res) {
-  client = new Fitbit(
-        process.env.KEY
-      , process.SECRET
-      , { // Now set with access tokens
-            accessToken: req.session.oauth.accessToken
-          , accessTokenSecret: req.session.oauth.accessTokenSecret
-          , unitMeasure: 'en_GB'
-        }
-    );  // Fetch todays activities
-  client.getActivities(function (err, activities) {
-    if (err) {
-      // Take action
-      return;
-    }
-
-    // `activities` is a Resource model
-    res.send('Total steps today: ' + activities.steps());
-  });
-});
-
-
-
 
 app.get('/styleguide', function (req, res) {
   res.render('styleguide')
@@ -182,20 +104,6 @@ app.get('/styleguide', function (req, res) {
 app.get('/', function(req, res){
   res.render('index', { user: req.user });
 });
-
-
-app.get('/login', function(req, res){
-  res.render('login', { user: req.user });
-});
-
-
-app.get('/logout', function(req, res){
-  req.logout();
-  res.redirect('/');
-});
-
-
-
 
 
 // catch 404 and forward to error handler
@@ -233,24 +141,5 @@ function ensureAuthenticated(req, res, next) {
   if (req.isAuthenticated()) { return next(); }
   res.redirect('/login')
 }
-
-
-
-
-// var client = new Fitbit(process.env.KEY, process.env.SECRET);
-//
-// client.getRequestToken(function (err, token, tokenSecret) {
-//   if (err) {
-//     res.redirect('/')
-//     // Take action
-//     // return;
-//   }
-//
-//   req.session.oauth = {
-//       requestToken: token
-//     , requestTokenSecret: tokenSecret
-//   };
-//   res.redirect(client.authorizeUrl(token));
-
 
 module.exports = app;
